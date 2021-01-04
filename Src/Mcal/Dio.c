1297bb02515108c15fdc15f257ff43899fd9c692
Ahmed desawi <ahmed.desawi@gmail.com>
2021-01-05 00:17:32 +0200

    GPIO Folder structure

---
+++ Src/Mcal/Dio.c
@@ -0,0 +1,34 @@
+/*
+ * Dio.c
+ *
+ *  Created on: Jan 5, 2021
+ *      Author: Ahmed Ibrahim
+ */
+
+#include "Dio.h"
+
+Dio_LevelType Dio_ReadChannel (Dio_ChannelType ChannelId)
+{
+	Dio_LevelType RetValue;
+
+	return RetValue;
+}
+void Dio_WriteChannel (Dio_ChannelType ChannelId, Dio_LevelType Level)
+{
+
+}
+Dio_PortLevelType Dio_ReadPort (Dio_PortType PortId)
+{
+
+}
+void Dio_WritePort (Dio_PortType PortId, Dio_PortLevelType Level)
+{
+
+}
+Dio_LevelType Dio_FlipChannel (Dio_ChannelType ChannelId)
+{
+	Dio_LevelType RetValue;
+
+	return RetValue;
+}
+
---
+++ Src/Mcal/Inc/Dio.h
@@ -0,0 +1,21 @@
+/*
+ * Dio.h
+ *
+ *  Created on: Jan 5, 2021
+ *      Author: Ahmed Desawi
+ */
+
+#ifndef MCAL_INC_DIO_H_
+#define MCAL_INC_DIO_H_
+
+#include "stdint.h"
+
+
+typedef uint8_t Dio_PortType;
+typedef uint16_t Dio_ChannelType;
+typedef uint8_t Dio_LevelType;
+typedef uint16_t Dio_PortLevelType;
+
+
+
+#endif /* MCAL_INC_DIO_H_ */
