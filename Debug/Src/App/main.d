429a6d059149ff87175b006038c6b120e20f1042
Ahmed desawi <ahmed.desawi@gmail.com>
2021-01-04 23:39:33 +0200

    revert to delete new branch

--- Debug/Src/App/main.d
+++ Debug/Src/App/main.d
@@ -1,4 +1,1 @@
-Src/App/main.o: ../Src/App/main.c \
- C:/Users/HP/Desktop/Sprint/ARM_Architecture/Sprints_Repo/Sprints_Tasks/Src/Mcal/Inc/IntCtrl.h
-
-C:/Users/HP/Desktop/Sprint/ARM_Architecture/Sprints_Repo/Sprints_Tasks/Src/Mcal/Inc/IntCtrl.h:
+Src/App/main.o: ../Src/App/main.c
